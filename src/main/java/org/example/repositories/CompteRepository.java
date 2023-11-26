c4bb167c08cc2fc026128ed215b2ba9996a27cd0
mhdghb <mahdi.elgharbi2@gmail.com>
2023-11-26 21:56:15 +0000

    init

--- src/main/java/org/example/repositories/CompteRepository.java
+++ src/main/java/org/example/repositories/CompteRepository.java
@@ -1,9 +1,12 @@
 package org.example.repositories;
 
-import entities.Compte;
+import org.example.entites.Compte;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Date;
+
 @Repository
 public interface CompteRepository extends JpaRepository<Compte, Long> {
+    //void save(int i, double v , Date date, Compte.typeCompte typecompte, Compte.etatCompte etatcompte);
 }
--- src/main/java/org/example/web/MahdiGharbiLsi3Tp6Application.java
+++ src/main/java/org/example/web/MahdiGharbiLsi3Tp6Application.java
@@ -1,12 +1,12 @@
 package org.example.web;
-        import entities.Compte;
+        import org.example.entites.Compte;
         import org.springframework.boot.CommandLineRunner;
         import org.springframework.boot.SpringApplication;
         import org.springframework.boot.autoconfigure.SpringBootApplication;
         import org.springframework.boot.autoconfigure.domain.EntityScan;
         import org.springframework.context.annotation.Bean;
         import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
-        import repositories.CompteRepository;
+        import org.example.repositories.CompteRepository;
         import java.util.Date;
 @SpringBootApplication
 @EnableJpaRepositories(basePackages = "repositories") // Active la configuration des dépôts JPA
@@ -46,7 +46,6 @@ public class MahdiGharbiLsi3Tp6Application {
             compteRepository.save(compte1);
             compteRepository.save(compte2);
             compteRepository.save(compte3);
-
             // Affichage des soldes des comptes enregistrés dans la console
             compteRepository.findAll().forEach(compte -> System.out.println(" compte #" + compte.getId() + ": " +"  Type de compte:  "+compte.getType()+"  Etat de compte:  "+compte.getEtat() + "  Solde du compte :"+compte.getSolde()));
 
